9eb2f3a1e25be654cf97aa5432bd0255b03c61a6
Kiet <kietngo255@gmail.com>
2023-08-24 18:52:50 +0700

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-// Replace this with your MONGOURI.
-const MONGOURI = "mongodb+srv://kiet:kiet@kiet.ov07xay.mongodb.net/";
+// Replace this with your MONGOURL.
+const MONGOURI = "";
 
 const InitiateMongoServer = async () => {
     try {
